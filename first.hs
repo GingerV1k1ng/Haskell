e48cbd442ca66d6ba7839aeee9b4e3be0d77beff
GingerV1k1ng <thorhuga1@live.dk>
2016-09-15 12:24:24 +0200

    Uploading my first.hs and calculator.hs
    Uploading the progress i have with my haskell code, calculator is a
    attempt on making a GUI calculator using haskell and GTK+ library

---
+++ first.hs
@@ -0,0 +1,42 @@
+
+
+
+
+doubleMe x = x + x
+
+
+
+
+doubleUs x y = doubleMe x*2 - doubleMe y*2
+
+foo x = x + x + x + x + x + x +
+        x + x + x
+
+
+
+doubleSmallNumber x = if x > 100
+then x else x*2
+
+
+
+
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+
+
+
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
