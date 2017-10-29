51bf4f0516f2593276e1a669fafc55292c3f0ae6
githuis <zhex@live.dk>
2017-10-29 17:56:41 +0100

    Add 2.5 of the last 3 excercises in session 2

--- Sessions/Session_2.hs
+++ Sessions/Session_2.hs
@@ -25,7 +25,33 @@ nat2int (Succ(n)) = 1 + nat2int n
 int2nat 0 = Zero
 int2nat n = Succ(int2nat n)
 
-data Tree a = Leaf a | Node (Tree a) (Tree a)
+-- fib usng lazy evaluation
+
+magic 0 _ = []
+magic m n = m : (magic n (m+n))
+
+getIt :: [Int] -> Int -> Int
+getIt []     _ = undefined
+getIt (x:xs) 1 = x
+getIt (x:xs) n = getIt xs (n-1)
+
+fib n = getIt (magic 1 1) n
+
+--check if tree is balanced
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
+
+badTree = Node (Leaf 5) (Node (Leaf 5) (Node (Leaf 17) (Leaf 18)))
+goodTree = Node (Leaf 5) (Node (Leaf 5)  (Leaf 17))
 
 balanced (Leaf a) = True
-balanced (Node a1 a2) = balanced a1 == balanced a2
+balanced (Node a b) = let x = leaves a in let y = leaves b in diff x y && balanced a && balanced b
+
+leaves (Leaf n) = 1
+leaves (Node a b) = leaves a + leaves b
+
+diff n m = abs(n - m) <= 1
+
+-- the start of constructing a balanced tree from a list
+
+balance xs = (Leaf xs)
+balance (x:xs) = let l = length (x:xs) in if l > 2 then Node (balance take l/2 (x:xs)) (balance drop l/2 (x:xs))  else (Node (Leaf x) (Leaf xs))
